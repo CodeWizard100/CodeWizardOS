03e7438b50f97e314117c710c6f0327f794471e4
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-07-12 19:05:36 +0200

    Update system.h

--- src/bootloader/stage2/system.h
+++ src/bootloader/stage2/system.h
@@ -3,29 +3,18 @@
 
 #include "stdint.h"
 
-uint8_t inportb(uint16_t port) {
-    uint8_t value;
+uint8_t inportb(uint16_t port);
+void outportb(uint16_t port, uint8_t data);
 
-    // Use #pragma aux to define inline assembly block
-    #pragma aux inport = \
-        "in al, dx"   /* Assembly instruction to read from port */ \
-        "mov byte ptr [value], al" /* Move result to value */ \
-        parm [dx] \
-        value [al];
+#pragma aux inportb = \
+    "in al, dx" \
+    "mov byte ptr [eax], al" \
+    parm [dx] \
+    modify [al] \
+    value [al];
 
-    // Call the inline assembly function
-   
-
-    // Return the value read from the port
-    return value;
-}
-
-void outportb(uint16_t port, uint8_t data) {
-  // Function definition using Watcom      inline assembly
-  #pragma aux outport = \
-    "out dx, al"   /* Assembly   instruction to write to port */ \
+#pragma aux outportb = \
+    "out dx, al" \
     parm [dx] [al];
-}
-
 
 #endif // SYSTEM_H
