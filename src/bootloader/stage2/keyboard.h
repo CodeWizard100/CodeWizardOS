6b60a557d60d15131a32346b344d144ed07c3dd9
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-07-12 18:54:31 +0200

    Update keyboard.h

--- src/bootloader/stage2/keyboard.h
+++ src/bootloader/stage2/keyboard.h
@@ -5,11 +5,20 @@
 
 #include "stdint.h"
 
-char* getinput() {
+#include "stdio.h"
+
+void getinput() {
   char buff;
   char* buffstr;
   uint8_t i = 0;
   uint8_t reading = 1;
+
+  while(reading)
+  {
+    if (inportb(0x64) & 0x1) {
+      puts("I think ypu clicked something");
+    }
+  }
 }
 
 #endif // KEYBOARD_H
