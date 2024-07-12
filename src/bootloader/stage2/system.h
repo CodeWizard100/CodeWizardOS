c6ab16000392b9f9fcbfeea45d382bcf9a382cf3
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-07-12 13:38:58 +0200

    Update system.h

--- src/bootloader/stage2/system.h
+++ src/bootloader/stage2/system.h
@@ -5,12 +5,12 @@
 
 static inline uint8 inportb(uint16 _port) {
     uint8 rv;
-    asm volatile ("inb %1, %0" : "=a" (rv) : "dN" (_port));
+    __asm__ volatile ("inb %1, %0" : "=a" (rv) : "dN" (_port));
     return rv;
 }
 
 static inline void outportb(uint16 _port, uint8 _data) {
-    asm volatile ("outb %1, %0" : : "dN" (_port), "a" (_data));
+    __asm__ volatile ("outb %1, %0" : : "dN" (_port), "a" (_data));
 }
 
 #endif // SYSTEM_H
