8dea13797a33e5778035167c1c0b9dae72c93ccb
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-07-12 11:11:01 +0200

    Update main.c

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -6,8 +6,10 @@ void _cdecl cstart_(uint16_t bootDrive)
     const char far* far_str = "far string";
 
     puts("Hello world from C!\r\n");
-    printf("Formatted %% %c %s %ls\r\n", 'a', "string", far_str);
-    printf("Formatted %d %i %x %p %o %hd %hi %hhu %hhd\r\n", 1234, -5678, 0xdead, 0xbeef, 012345, (short)27, (short)-42, (unsigned char)20, (signed char)-10);
-    printf("Formatted %ld %lx %lld %llx\r\n", -100000000l, 0xdeadbeeful, 10200300400ll, 0xdeadbeeffeebdaedull);
-    for (;;);
+    
+    char* vid = (char*)0xB8000;
+
+    vid[0] = 'A';
+
+    vid[1] = 0x07;
 }
