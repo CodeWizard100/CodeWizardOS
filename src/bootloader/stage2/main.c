bf8308678ff51aa09ae6eb8ff8647323c956d8f7
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-07-12 19:36:39 +0200

    Update main.c

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -1,14 +1,9 @@
 #include "stdint.h"
 #include "stdio.h"
-#include "keyboard.h"
 
 void _cdecl cstart_(uint16_t bootDrive)
 {
     const char far* far_str = "far string";
 
     puts("Hello world from C!\r\n");
-
-   while (1) {
-    getinput();
-  }
 }
