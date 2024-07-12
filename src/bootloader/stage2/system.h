369f39186788df5d1c50c7960800b1894f3d7827
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-07-12 13:17:42 +0200

    Update system.h

--- src/bootloader/stage2/system.h
+++ src/bootloader/stage2/system.h
@@ -1,6 +1,19 @@
-#indef SYSTEM_H
-#define SYSTEM_H
+
+#ifndef SYSTEM_H
+#define SYSTEM H
+
 #include "types.h"
 
-#endif
+uint8 inportb (uint16_port)
+{
+uint8 rv;
+_asm__volatile ("inb %1, %0" : "=a" (rv):"dN" (_port)); 
+return rv;
+}
 
+void outportb (uint16 _port, uint8_data)
+{
+asm _volatile ("outb %1, %0": : "dN" (_port), "a" (_data));
+}
+
+#endif
