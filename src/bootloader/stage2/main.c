2ca5f3f76913e6c0364186d7c2e75d28325414e9
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-07-12 13:54:01 +0200

    Update main.c

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -1,20 +1,22 @@
 #include "stdint.h"
 #include "stdio.h"
-#include "types.h"
 
-static inline uint8 inportb(uint16 _port) {
-    uint8 rv;
-    __asm__ volatile ("inb %1, %0" : "=a" (rv) : "dN" (_port));
-    return rv;
+void example_function() {
+    int value = 10;
+    __asm__ volatile (
+        "movl %0, %%eax;"
+        "addl $5, %%eax;"
+        : /* output operands */
+        : "r" (value)
+        : "%eax" /* clobbered registers */
+    );
+
+    printf("Result: %d\n", value);
 }
-
-static inline void outportb(uint16 _port, uint8 _data) {
-    __asm__ volatile ("outb %1, %0" : : "dN" (_port), "a" (_data));
-}
-
 
 void _cdecl cstart_(uint16_t bootDrive)
 {
+example_function();
     const char far* far_str = "far string";
 
     puts("Hello world from C!\r\n");
