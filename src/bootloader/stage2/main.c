db195a4dc6a77c34a3771712a44668e7570c0f27
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-07-12 11:17:58 +0200

    Update main.c

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -6,10 +6,4 @@ void _cdecl cstart_(uint16_t bootDrive)
     const char far* far_str = "far string";
 
     puts("Hello world from C!\r\n");
-    
-    char* vidmem = (char*)0xB8000;
-
-    vidmem[0] = 'A';
-
-    vidmem[1] = 0x07;
 }
