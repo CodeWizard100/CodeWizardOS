813c0b402454ff3507a1cc59b16c274bec1c3195
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-07-12 15:17:05 +0200

    Update main.c

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -1,24 +1,6 @@
 #include "stdint.h"
 #include "stdio.h"
 
-// Function to read a byte from an I/O port using Watcom inline assembly
-uint8_t inportb(uint16_t port) {
-    uint8_t value;
-
-    // Use #pragma aux to define inline assembly block
-    #pragma aux inport = \
-        "in al, dx"   /* Assembly instruction to read from port */ \
-        "mov byte ptr [value], al" /* Move result to value */ \
-        parm [dx] \
-        value [al];
-
-    // Call the inline assembly function
-   
-
-    // Return the value read from the port
-    return value;
-}
-
 void _cdecl cstart_(uint16_t bootDrive)
 {
     const char far* far_str = "far string";
