9034510e26ecb1e07401f33f140b12c8ef29dfa0
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-07-12 14:57:34 +0200

    Update main.c

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -1,7 +1,17 @@
 #include "stdint.h"
 #include "stdio.h"
 
+// Function to read a byte from an I/O port
+uint8_t inportb(uint16_t port) {
+    // Declare a variable to hold the value read from the port
+    uint8_t value;
 
+    // Perform the input operation using inline assembly
+    asm volatile ("inb %1, %0" : "=a" (value) : "dN" (port));
+
+    // Return the value read from the port
+    return value;
+}
 
 void _cdecl cstart_(uint16_t bootDrive)
 {
