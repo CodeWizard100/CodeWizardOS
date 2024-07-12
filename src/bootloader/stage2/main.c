b8d67d385145f7a95a022a716e3c8771f36cff03
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-07-12 14:07:06 +0200

    Update main.c

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -1,22 +1,10 @@
 #include "stdint.h"
 #include "stdio.h"
 
-void example_function() {
-    int value = 10;
-    __asm__ volatile (
-        "movl %0, %%eax;"
-        "addl $5, %%eax;"
-        : /* output operands */
-        : "r" (value)
-        : "%eax" /* clobbered registers */
-    );
 
-    printf("Result: %d\n", value);
-}
 
 void _cdecl cstart_(uint16_t bootDrive)
 {
-example_function();
     const char far* far_str = "far string";
 
     puts("Hello world from C!\r\n");
