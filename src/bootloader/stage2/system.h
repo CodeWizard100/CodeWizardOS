b926f32ac4e5c3cd174f81c6513c210f17e4c9a8
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-07-12 13:09:19 +0200

    Update system.h

--- src/bootloader/stage2/system.h
+++ src/bootloader/stage2/system.h
@@ -1,19 +1,6 @@
 #indef SYSTEM_H
 #define SYSTEM_H
-
-typedef signed char int8;
-typedef unsigned char uint8;
-
-typedef signed short int16;
-typedef unsigned short uint16;
-
-typedef signed int int32;
-typedef unsigned int uint32;
-
-typedef signed long long int64;
-typedef unsigned long long uint64;
-
-typedef char* string;
+#include "types.h"
 
 #endif
 
