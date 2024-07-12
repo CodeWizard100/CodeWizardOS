810cefdd7506ce538d39880a72bf46149ed12210
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-07-12 13:46:45 +0200

    Update main.c

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -3,6 +3,17 @@
 #include "system.h"
 #include "types.h"
 
+static inline uint8 inportb(uint16 _port) {
+    uint8 rv;
+    __asm__ volatile ("inb %1, %0" : "=a" (rv) : "dN" (_port));
+    return rv;
+}
+
+static inline void outportb(uint16 _port, uint8 _data) {
+    __asm__ volatile ("outb %1, %0" : : "dN" (_port), "a" (_data));
+}
+
+
 void _cdecl cstart_(uint16_t bootDrive)
 {
     const char far* far_str = "far string";
