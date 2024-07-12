c2f194167edcfc81b06ecc37a1235894c8ca1a5f
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-07-12 18:47:43 +0200

    Update keyboard.h

--- src/bootloader/stage2/keyboard.h
+++ src/bootloader/stage2/keyboard.h
@@ -1,4 +1,13 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
+#include "system.h"
+
+char* getinput() {
+  char buff;
+  char* buffstr;
+  uint8_t i = 0;
+  uint8_t reading = 1;
+}
+
 #endif // KEYBOARD_H
